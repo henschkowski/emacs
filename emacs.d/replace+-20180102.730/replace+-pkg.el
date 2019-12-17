63c6837c07d870a674be5c1614ac6aba3715e3ad
Ralf Henschkowski <ralf.henschkowski@swisscom.com>
2019-12-17 15:48:56 +0100

    some deletions and additions

--- emacs.d/replace+-20180102.730/replace+-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "replace+" "20180102.730" "Extensions to `replace.el'." 'nil :commit "70e2b63829a1674345b0c522ca49494d067c903e" :keywords '("matching" "help" "internal" "tools" "local") :authors '(("Drew Adams")) :maintainer '(nil . "Drew Adams (concat \"drew.adams\" \"@\" \"oracle\" \".com\")") :url "https://www.emacswiki.org/emacs/download/replace%2b.el")
